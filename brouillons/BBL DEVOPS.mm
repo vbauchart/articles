f6a05e6ec1b091b32bf6a69ca8981e568cdbae07
Vincent BAUCHART <vincent-externe.bauchart@dalkia.fr>
2023-10-23 10:32:08 +0200

    add freemind maps

---
+++ brouillons/BBL DEVOPS.mm
@@ -0,0 +1,264 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1696326895697" ID="ID_1174904068" LINK="BBL%20DEVOPS%20CRAFT.mm" MODIFIED="1696334973698" TEXT="BBL">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1697792242641" ID="ID_1382418307" MODIFIED="1697792250863" POSITION="left" TEXT="pr&#xe9;sentation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1697792251907" ID="ID_782974737" MODIFIED="1697792273091" TEXT="16ans infra applicative">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1697789737331" ID="ID_1981351152" MODIFIED="1697789742278" POSITION="left" TEXT="Question pour Hermes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1697792777268" ID="ID_986550706" MODIFIED="1697792786719" TEXT="pr&#xe9;ciser DEVOPS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1697792787203" ID="ID_354423658" MODIFIED="1697792790063" TEXT="Public">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1697789743289" ID="ID_1368760690" MODIFIED="1697789981072" TEXT="Quelle technologie de d&#xe9;ploiement">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1697789746985" ID="ID_272301060" MODIFIED="1697789945133" TEXT="Quel fournisseur d&apos;infra">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1697789946144" ID="ID_997962093" MODIFIED="1697789994373" TEXT="Quel fournisseur de CI">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1697789750633" ID="ID_62275206" MODIFIED="1697789927915" TEXT="la plus grosse probl&#xe9;matique ressentie">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1697790023354" ID="ID_1264696458" MODIFIED="1697790026554" TEXT="Orga">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1697790026817" ID="ID_1693291860" MODIFIED="1697790031093" TEXT="Technique">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1697790015520" ID="ID_1956673316" MODIFIED="1697790020558" TEXT="les plus gros irritants connu">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1697790678882" ID="ID_1957287905" MODIFIED="1697790762492" TEXT="pendant le BUILD">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1697790683048" ID="ID_1936744630" MODIFIED="1697790757347" TEXT="pendant le DEPLOY">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1697790685737" ID="ID_650665518" MODIFIED="1697790752150" TEXT="pendant le RUN">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1696326903256" FOLDED="true" ID="ID_115188667" MODIFIED="1697792770271" POSITION="left" TEXT="Probl&#xe9;matique">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1696326906453" ID="ID_968319606" MODIFIED="1696334973681" TEXT="Silos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696326912405" ID="ID_1156626837" MODIFIED="1696334973681" TEXT="de responsabilit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696326920138" ID="ID_1661918556" MODIFIED="1696334973681" TEXT="de comp&#xe9;tence">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331313070" ID="ID_867401732" MODIFIED="1696334973682" TEXT="d&apos;expertise">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696326931977" ID="ID_1245598230" MODIFIED="1696334973682" TEXT="Coordination simple ne marche pas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696334619054" ID="ID_714662282" MODIFIED="1696334973682" TEXT="DeVoPs C&apos;eSt UnE CuLtUrE">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1696326978946" FOLDED="true" ID="ID_1671943002" MODIFIED="1697792769169" POSITION="left" TEXT="La solution">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1697785226591" ID="ID_1710297841" MODIFIED="1697785234253" TEXT="Le meilleur SILO que vous connaissez">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1697785234843" ID="ID_732086091" MODIFIED="1697785243909" TEXT="AWS/GCP/AZURE">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1697785246498" ID="ID_535667874" MODIFIED="1697785272228" TEXT="Avez-vous d&#xe9;j&#xe0; parl&#xe9; &#xe0; un DEVOPS de AWS ?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696326998827" ID="ID_1698328372" MODIFIED="1696334973683" TEXT="Single Responsability">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1696327007753" ID="ID_665304771" MODIFIED="1696334973685" TEXT="Contrat">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1696327026898" ID="ID_432997888" MODIFIED="1696334973685" TEXT="Plateformisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696327036346" ID="ID_89897987" MODIFIED="1696334973685" TEXT="Centre de comp&#xe9;tence">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331116044" ID="ID_878564630" MODIFIED="1696334973685" TEXT="Contrat par Jira">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696327032371" ID="ID_1206801528" MODIFIED="1696334973686" TEXT="contrat par API">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1696331134511" FOLDED="true" ID="ID_298594" MODIFIED="1697790037598" POSITION="right" TEXT="Exemple certificat SSL As A service">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1696331322345" ID="ID_683158177" MODIFIED="1696334973688" TEXT="Proc&#xe9;dure nouveau site HTTPS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696334931604" ID="ID_737601120" MODIFIED="1696334973688" TEXT="L&apos;&#xe9;quipe LB s&apos;occupe d&apos;un point d&apos;entr&#xe9;e unique">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331361205" ID="ID_549540684" MODIFIED="1696334973689" TEXT="Par s&#xe9;curit&#xe9; l&apos;&#xe9;quipe LB ne doit pouvoir &#xea;tre capable de cr&#xe9;er des certificat">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696333927845" ID="ID_1671801484" MODIFIED="1696334408832" TEXT="s&#xe9;curit&#xe9; par s&#xe9;paration des pouvoirs"/>
+<node COLOR="#111111" CREATED="1696333931908" ID="ID_695608216" MODIFIED="1696333941911" TEXT="le fameux &quot;Juge et Parti&quot;"/>
+<node COLOR="#111111" CREATED="1696334556988" ID="ID_108802858" MODIFIED="1696334973689" TEXT="(mais est-ce bien de la s&#xe9;curit&#xe9;?)"/>
+</node>
+<node COLOR="#990000" CREATED="1696331382549" ID="ID_1536994350" MODIFIED="1696334973689" TEXT="Le metier d&#xe9;cide de l&apos;URL et est donc responsable de la cr&#xe9;ation du certificat">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331400415" ID="ID_966683978" MODIFIED="1696334973689" TEXT="Le metier doit faire une demande de certificat aupres de l&apos;&#xe9;quipe PKI">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331420052" ID="ID_1092018941" MODIFIED="1696334973689" TEXT="Une fois la clef priv&#xe9;e et le certificat obtenu, il doit faire une demande d&apos;installation sur le LB">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331469782" ID="ID_1764921629" MODIFIED="1696334973690" TEXT="L&apos;&#xe9;quipe LB installe le certificat et configure la redirection">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696331504807" ID="ID_772246499" MODIFIED="1696334973690" TEXT="Probl&#xe8;me">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696332028835" ID="ID_1749212585" MODIFIED="1696334973690" TEXT="Le metier doit connaitre la proc&#xe9;dure alors qu&apos;il n&apos;est pas expert">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696332039428" ID="ID_266721883" MODIFIED="1696332041405" TEXT="Confluence"/>
+<node COLOR="#111111" CREATED="1696332089436" ID="ID_160984476" MODIFIED="1696332091158" TEXT="Sharepoint"/>
+</node>
+<node COLOR="#990000" CREATED="1696331572277" ID="ID_1496848093" MODIFIED="1696334973691" TEXT="Lors du transit de la clef, risque de disclosure">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696331751566" ID="ID_260025633" MODIFIED="1696331760818" TEXT="On rappelle qu&apos;un certificat sert &#xe0; authentifier un serveur"/>
+</node>
+<node COLOR="#990000" CREATED="1696334275899" ID="ID_978693827" MODIFIED="1696334973691" TEXT="L&apos;&#xe9;quipe LB aura la clef priv&#xe9;e au final">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696331627735" ID="ID_462735433" MODIFIED="1696334973691" TEXT="L&apos;&#xe9;quipe LB est &#xe9;galement responsable de configurer la redirection vers l&apos;appli">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696334218030" ID="ID_726663583" MODIFIED="1696334973691" TEXT="risque de desynchro URL certificat et URL configur&#xe9;"/>
+<node COLOR="#111111" CREATED="1696334383580" ID="ID_1443830478" MODIFIED="1696334973691" TEXT="A finalement tout le pouvoir"/>
+</node>
+<node COLOR="#990000" CREATED="1696331677263" ID="ID_1481477539" MODIFIED="1696334973692" TEXT="2 entit&#xe9; impliqu&#xe9;s dans la m&#xea;me feature">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696331926943" ID="ID_750752973" MODIFIED="1696331965340" TEXT="rappel de la feature : un browser doit pouvoir se connecter &#xe0; l&apos;application en SSL"/>
+<node COLOR="#111111" CREATED="1696334439990" ID="ID_1107312007" MODIFIED="1696334973692" TEXT="Etendre oralement aux &#xe9;quipes DNS et Firewall"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696333723035" ID="ID_1609458752" MODIFIED="1696334973692" TEXT="Cons&#xe9;quence">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696331980298" ID="ID_741045478" MODIFIED="1696334973693" TEXT="On vient de cr&#xe9;er de la bureaucratie">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696331990251" ID="ID_1979942698" MODIFIED="1696332004812" TEXT="l&apos;organisation n&apos;est pas align&#xe9;e avec la technique"/>
+<node COLOR="#111111" CREATED="1696332007225" ID="ID_1028405708" MODIFIED="1696332013389" TEXT="Pourtant partait d&apos;une bonne intention">
+<node COLOR="#111111" CREATED="1696332014475" ID="ID_1055214509" MODIFIED="1696332020123" TEXT="l&apos;URL appartient au metier"/>
+<node COLOR="#111111" CREATED="1696334307187" ID="ID_576557064" MODIFIED="1696334973693" TEXT="S&#xe9;paration des pouvoirs"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1696333298748" ID="ID_206048614" MODIFIED="1696334973693" TEXT="Pas possible d&apos;automatiser car reponsabilit&#xe9; dillu&#xe9;e">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696332048945" ID="ID_1399773623" MODIFIED="1696334973696" TEXT="Solution : Delegation de responsabilit&#xe9;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696332059234" ID="ID_504623946" MODIFIED="1696334973696" TEXT="Le metier fait une demande unique a l&apos;&#xe9;quipe LB">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696332071941" ID="ID_889568710" MODIFIED="1696332128199" TEXT="Merci de configurer l&apos;URL en HTTPS et rediriger vers mon application"/>
+</node>
+<node COLOR="#990000" CREATED="1696333076890" ID="ID_687745114" MODIFIED="1696334973696" TEXT="l&apos;&#xe9;quipe LB s&apos;assure de l&apos;authenticit&#xe9; de la demande">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696333089342" ID="ID_8469388" MODIFIED="1696334497416" TEXT="SecOPS ;-)"/>
+</node>
+<node COLOR="#990000" CREATED="1696334857787" ID="ID_1074209444" MODIFIED="1696334973696" TEXT="Seul l&apos;&#xe9;quipe LB a le droit de faire des demandes &#xe0; l&apos;&#xe9;quipe PKi">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696333501627" ID="ID_957601079" MODIFIED="1696334973696" TEXT="Un contrat est cr&#xe9;&#xe9; entre LBPKI et les metiers">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696333332468" ID="ID_1367904974" MODIFIED="1696333335871" TEXT="1 seul ticket JIRA"/>
+<node COLOR="#111111" CREATED="1696335210235" ID="ID_874752028" MODIFIED="1696335210237" TEXT=""/>
+</node>
+<node COLOR="#990000" CREATED="1696335174922" ID="ID_704945013" MODIFIED="1696335186213" TEXT="Pose la question de la fusion des &#xe9;quipes LB et PKI ??">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1696332135489" ID="ID_795013526" MODIFIED="1696334973697" TEXT="Phase Automatisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696333228971" ID="ID_1667577346" MODIFIED="1696334973697" TEXT="l&apos;URL doit &#xea;tre identique dans le certificat et la configuration du LB">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696333256158" ID="ID_1286462908" MODIFIED="1696333271492" TEXT="Script qui prend en entr&#xe9;e">
+<node COLOR="#111111" CREATED="1696333273022" ID="ID_1783522690" MODIFIED="1696333274822" TEXT="URL"/>
+<node COLOR="#111111" CREATED="1696333275291" ID="ID_1608985704" MODIFIED="1696333282268" TEXT="Serveurs:Port de l&apos;application"/>
+</node>
+<node COLOR="#111111" CREATED="1696333763948" ID="ID_1786775420" MODIFIED="1696334680311" TEXT="G&#xe9;n&#xe8;re un certificat gr&#xe2;ce &#xe0; une API externe"/>
+<node COLOR="#111111" CREATED="1696333786163" ID="ID_33409178" MODIFIED="1696333790408" TEXT="G&#xe9;nere la conf"/>
+</node>
+<node COLOR="#990000" CREATED="1696334660612" ID="ID_1515231081" MODIFIED="1696334973697" TEXT="Lib&#xe9;ration par la technique">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696334689708" ID="ID_542600584" MODIFIED="1696334973698" TEXT="Protocle ACME (Lets Encrypt)">
+<node COLOR="#111111" CREATED="1696334682093" ID="ID_1265088014" MODIFIED="1696334973698" TEXT="traefik"/>
+<node COLOR="#111111" CREATED="1696334684844" ID="ID_109347869" MODIFIED="1696334973698" TEXT="Caddy"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1696333793348" ID="ID_1690696508" MODIFIED="1696334973698" TEXT="Cr&#xe9;ation d&apos;un provider terraform">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696333819284" ID="ID_1625560024" MODIFIED="1696333833174" TEXT="n&#xe9;c&#xe9;ssaire quand les demandes sont nombreuses et sensibles"/>
+<node COLOR="#111111" CREATED="1696333804349" ID="ID_1547924159" MODIFIED="1696333806637" TEXT="plus de JIRA"/>
+<node COLOR="#111111" CREATED="1696333806866" ID="ID_186443990" MODIFIED="1696333811463" TEXT="Contrat technique fort"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1696334987347" FOLDED="true" ID="ID_1170367126" MODIFIED="1697790042621" POSITION="right" TEXT="Exemple Deploiement As a Service">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1696335000692" ID="ID_77157121" MODIFIED="1696335008751" TEXT="Deployer une application en production">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1696335015653" ID="ID_884642861" MODIFIED="1696335027743" TEXT="l&apos;&#xe9;quipe OPS a les droits sur la production">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696335028252" ID="ID_953875535" MODIFIED="1696335036080" TEXT="Securit&#xe9; par s&#xe9;paration des pouvoirs"/>
+<node COLOR="#111111" CREATED="1696335039443" ID="ID_1489444592" MODIFIED="1696335046381" TEXT="Responsabilit&#xe9; du SLA"/>
+</node>
+<node COLOR="#990000" CREATED="1696335053035" ID="ID_1692966759" MODIFIED="1696335072055" TEXT="l&apos;&#xe9;quipe DEV fait une demande d&apos;installation en production">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1696335073229" ID="ID_630587994" MODIFIED="1696335093553" TEXT="l&apos;&#xe9;quipe fourni la documentation n&#xe9;cessaire">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1696335274012" ID="ID_810076007" MODIFIED="1696335277374" TEXT="Doc installation">
+<node COLOR="#111111" CREATED="1696335302121" ID="ID_1835997569" MODIFIED="1696335314117" TEXT="description fichier de configuration"/>
+</node>
+<node COLOR="#111111" CREATED="1696335282011" ID="ID_783102872" MODIFIED="1696335296295" TEXT="Proc&#xe9;dures d&apos;exploitations"/>
+</node>
+</node>
+</node>
+</node>
+</map>
